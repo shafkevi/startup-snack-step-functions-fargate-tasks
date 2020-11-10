aa9d2fc881d4176769a4a9c31cbe265e373ea458
Kevin Shaffer-Morrison <shafkevi@amazon.com>
2020-11-10 11:17:39 -0500

    updating main python to accept inputs and a quick script to run the state machine

--- src/lambda/decider/index.py
+++ src/lambda/decider/index.py
@@ -1,4 +1,7 @@
 import random
 
 def main(event, context):
-    return {"processor": random.choice(["both", "pi", "euler", "none"]), "digits": str(random.randint(1,25))}
+    if "processor" in event:
+        return {"processor": event["processor"], "digits": str(random.randint(1,25))}
+    else:
+        return {"processor": random.choice(["both", "pi", "euler", "none"]), "digits": str(random.randint(1,25))}
