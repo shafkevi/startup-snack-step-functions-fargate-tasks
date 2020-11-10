a4d389aba7f1693a3acf3de9c3428c4a9a690493
Ben Shank <benshank@amazon.com>
2020-11-10 08:41:59 -0600

    wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ set -e
 
 mkdir -p dist
 
+npx tsc
+
 rsync -azr --delete secrets/ dist/secrets/
 rsync -azr --delete src/ dist/src/
-rsync -azr --delete lib/utils/package.json dist/lib/utils/
 
-npx tsc
